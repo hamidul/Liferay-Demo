9db1ea898855f7b821fc02895cef34c6516f9226
hislam <hamidulasm@gmail.com>
2019-04-14 14:42:06 +0530

    initial commit

---
+++ sample-spring-portlet/src/main/java/com/proliferay/portlet/SampleSpringPortletViewController.java
@@ -0,0 +1,21 @@
+package com.proliferay.portlet;
+
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.portlet.bind.annotation.RenderMapping;
+
+/**
+ * @author hamidul
+ */
+@Controller
+@RequestMapping("VIEW")
+public class SampleSpringPortletViewController {
+
+	@RenderMapping
+	public String view(RenderRequest request, RenderResponse response) {
+		return "view";
+	}
+}
